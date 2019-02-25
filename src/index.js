2ee90ba8918354bc58f7c5d5530b7b9b41b4c5df
Gerasik <gerasik1992@gmail.com>
2019-02-26 00:24:19 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -4,4 +4,14 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var trianglesCount = 0;
+  for (var i = 0 ; i<preferences.length ;i++){
+    if (i+1 == preferences[preferences[preferences[i]-1]-1] & preferences[preferences[preferences[i]-1]-1] != preferences[preferences[i]-1]){
+      ++trianglesCount;
+      preferences[i]= 0;
+      preferences[preferences[i]-1] = 0;
+      preferences[preferences[preferences[i]-1]-1] = 0;
+    }  
+  } 
+  return trianglesCount;
 };
